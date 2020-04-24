d9b47e5e91a61e3bfc3bf8291d0a583ed355646e
taojianhao <taojianhao@foxmail.com>
2020-04-24 10:25:15 +0800

    zk redisson

---
+++ src/main/java/com/tjh/redis/RedisTest.java
@@ -0,0 +1,56 @@
+package com.tjh.redis;
+
+import jdk.nashorn.internal.ir.CallNode;
+import org.redisson.Redisson;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+
+import java.util.UUID;
+import java.util.concurrent.*;
+import java.util.concurrent.locks.ReentrantLock;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
+
+public class RedisTest {
+
+    private static Integer inventory = 1001;
+    private static final int NUM = 1000;
+    private static LinkedBlockingQueue linkedBlockingQueue = new LinkedBlockingQueue();
+    static RedisLock redisLock = new RedisLock();
+
+    public static void main(String[] args) {
+        ThreadPoolExecutor threadPoolExecutor =
+                new ThreadPoolExecutor(inventory, inventory, 10L, SECONDS, linkedBlockingQueue);
+        long start = System.currentTimeMillis();
+        Config config = new Config();
+        config.useSingleServer().setAddress("redis://127.0.0.1:6379");
+        final RedissonClient client = Redisson.create(config);
+        final RLock lock = client.getLock("lock1");
+        final CountDownLatch countDownLatch = new CountDownLatch(NUM+1);
+
+        for (int i = 0; i <= NUM; i++) {
+            threadPoolExecutor.execute(new Runnable() {
+                @Override
+                public void run() {
+                    //lock.lock();
+                    redisLock.lock(UUID.randomUUID().toString());
+                    inventory--;
+                    System.out.println(inventory);
+                    countDownLatch.countDown();
+                    //lock.unlock();
+                    redisLock.unlock(UUID.randomUUID().toString());
+                }
+            });
+        }
+        try {
+            countDownLatch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        long end = System.currentTimeMillis();
+        System.out.println("执行线程数:" + NUM + "   总耗时:" + (end - start) + "  库存数为:" + inventory);
+    }
+
+
+}
